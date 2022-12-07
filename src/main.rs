e5a644bcd310a014db2a6d0079656d2fc6a0ab83
Stirling Hostetter <stirlhoss@proton.me>
2022-12-07 11:47:15 -0600

    Add ctrl q to quit function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use std::io::{self, stdout, Read};
 use termion::raw::IntoRawMode;
 
+fn to_ctrl_byte(c: char) -> u8 {
+    let byte = c as u8;
+    byte & 0b0001_1111
+}
+
 fn main() {
     let _stdout = stdout().into_raw_mode().unwrap();
     for b in io::stdin().bytes(){
@@ -11,7 +16,7 @@ fn main() {
         } else {
             println!("{:?} ({})\r", b, c);
         }
-        if c == 'q' {
+        if b == to_ctrl_byte('q'){
             break;
         }
     }
