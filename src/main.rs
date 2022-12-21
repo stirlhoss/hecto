2abafb272e7d1a585bb2e6ce00b5c42e96024895
Stirling Hostetter <stirlhoss@proton.me>
2022-12-21 11:42:47 -0600

    Remove implementation from main

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,7 @@
-use std::io::{self, stdout};
-use termion::event::Key;
-use termion::input::TermRead;
-use termion::raw::IntoRawMode;
+mod editor;
+use editor::Editor;
 
-// fn to_ctrl_byte(c: char) -> u8 {
-//     let byte = c as u8;
-//     byte & 0b0001_1111
-// }
-
-fn die(e: std::io::Error) {
-    panic!("{}", e);
-}
-
-fn main() {
-    let _stdout = stdout().into_raw_mode().unwrap();
-    for key in io::stdin().keys(){
-        match key {
-            Ok(key) => match key {
-                Key::Char(c) => {
-                    if c.is_control(){
-                        println!("{:?} \r", c as u8);
-                    } else {
-                        println!("{:?} ({})\r", c as u8, c);
-                    }
-                }
-                Key::Ctrl('q') => break,
-                _ => println!("{:?}\r", key),
-            },
-            Err(err) => die(err),
-        }
-    }
+fn main(){
+    let editor = Editor {};
+    editor.run();
 }
