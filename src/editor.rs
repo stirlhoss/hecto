d9da56dfeb4992e4e158040317d7c330153a11b9
Stirling Hostetter <stirlhoss@proton.me>
2023-01-04 11:45:00 -0600

    Add struct for cursor position

--- src/editor.rs
+++ src/editor.rs
@@ -2,9 +2,16 @@ use crate::Terminal;
 use termion::event::Key;
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
+
+pub struct Position {
+   pub x: usize,
+   pub y: usize,
+}
+
 pub struct Editor{
     should_quit: bool,
     terminal: Terminal,
+    cursor_position: Position,
 }
 
 impl Editor {
@@ -25,18 +32,19 @@ impl Editor {
         Self{
             should_quit: false,
             terminal: Terminal::default().expect("Failed to initialize terminal"),
+            cursor_position: Position { x: 0, y: 0 },
         }
     }
 
     fn refresh_screen(&self) -> Result<(), std::io::Error> {
         Terminal::cursor_hide();
-        Terminal::cursor_position(0, 0);
+        Terminal::cursor_position(&Position {x: 0, y: 0 });
         if self.should_quit {
             Terminal::clear_screen();
             println!("Goodbye.\r");
         } else {
         self.draw_rows();
-            Terminal::cursor_position(0, 0);
+            Terminal::cursor_position(&self.cursor_position);
         }
         Terminal::cursor_show();
         Terminal::flush()
