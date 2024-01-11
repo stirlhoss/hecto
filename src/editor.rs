b19c47df7766f172428ae8044875d0925a361b67
Stirling Hostetter <stirlingh@simantel.com>
2024-01-11 13:42:24 -0600

    got to scrolling in chap 4

--- src/editor.rs
+++ src/editor.rs
@@ -1,23 +1,28 @@
+use crate::Document;
+use crate::Row;
 use crate::Terminal;
+use std::env;
 use termion::event::Key;
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
+#[derive(Default)]
 pub struct Position {
-   pub x: usize,
-   pub y: usize,
+    pub x: usize,
+    pub y: usize,
 }
 
-pub struct Editor{
+pub struct Editor {
     should_quit: bool,
     terminal: Terminal,
     cursor_position: Position,
+    document: Document,
 }
 
 impl Editor {
-    pub fn run(&mut self){
-        loop{
-            if let Err(error) = self.refresh_screen(){
+    pub fn run(&mut self) {
+        loop {
+            if let Err(error) = self.refresh_screen() {
                 die(error);
             }
             if self.should_quit {
@@ -29,28 +34,37 @@ impl Editor {
         }
     }
     pub fn default() -> Self {
-        Self{
+        let args: Vec<String> = env::args().collect();
+        let document = if args.len() > 1 {
+            let file_name = &args[1];
+            Document::open(&file_name).unwrap_or_default()
+        } else {
+            Document::default()
+        };
+
+        Self {
             should_quit: false,
             terminal: Terminal::default().expect("Failed to initialize terminal"),
-            cursor_position: Position { x: 0, y: 0 },
+            document,
+            cursor_position: Position::default(),
         }
     }
 
     fn refresh_screen(&self) -> Result<(), std::io::Error> {
         Terminal::cursor_hide();
-        Terminal::cursor_position(&Position {x: 0, y: 0 });
+        Terminal::cursor_position(&Position::default());
         if self.should_quit {
             Terminal::clear_screen();
             println!("Goodbye.\r");
         } else {
-        self.draw_rows();
+            self.draw_rows();
             Terminal::cursor_position(&self.cursor_position);
         }
         Terminal::cursor_show();
         Terminal::flush()
     }
     fn process_keypress(&mut self) -> Result<(), std::io::Error> {
-    let pressed_key = Terminal::read_key()?;
+        let pressed_key = Terminal::read_key()?;
         match pressed_key {
             Key::Ctrl('q') => self.should_quit = true,
             Key::Up
@@ -87,7 +101,7 @@ impl Editor {
             Key::PageDown => y = height,
             Key::Home => x = 0,
             Key::End => x = width,
-            _=> (),
+            _ => (),
         }
         self.cursor_position = Position { x, y }
     }
@@ -101,11 +115,19 @@ impl Editor {
         welcome_message.truncate(width);
         println!("{}\r", &welcome_message);
     }
+    pub fn draw_row(&self, row: &Row) {
+        let start = 0;
+        let end = self.terminal.size().width as usize;
+        let row = row.render(start, end);
+        println!("{}\r", row)
+    }
     fn draw_rows(&self) {
         let height = self.terminal.size().height;
-        for row in 0..height - 1 {
+        for terminal_row in 0..height - 1 {
             Terminal::clear_current_line();
-            if row == height / 3{
+            if let Some(row) = self.document.row(terminal_row as usize) {
+                self.draw_row(row);
+            } else if self.document.is_empty() && terminal_row == height / 3 {
                 self.draw_welcome_message();
             } else {
                 println!("~\r");
@@ -116,5 +138,5 @@ impl Editor {
 
 fn die(e: std::io::Error) {
     Terminal::clear_screen();
-    panic!("{}",e);
+    panic!("{}", e);
 }
